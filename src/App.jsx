2a2bc8be34090ca3ef460de2bc9df1d3c2a1b17f
AbdulRehmanjr <abdulrehman2020white@gmail.com>
2024-05-19 06:37:37 +0500

    fix(firebase): export const fixed , fix(chatapp.js):database access through firebase modified,

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import SignIn from './components/SignIn';
